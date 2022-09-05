69164f7afc473b5b38b36164dcfb648d700648f0
Alessandro Mondelli <alexs98@hotmail.it>
2022-09-05 18:39:03 +0200

    Fixate funzioni di scrittura ed eliminazione testo

--- components/contatti/ContattiHero.vue
+++ components/contatti/ContattiHero.vue
@@ -1,26 +1,41 @@
 <template>
     <div class="contatti-hero-wrap">
-        <p id="hero-content" class="contatti-hero-info"><span id="typing"></span><span class="writing-cursor">|</span></p>
+        <p id="hero-content" class="contatti-hero-info"><span id="typing" ref="typing" @click="eraseMessage">{{ message }}</span><span class="writing-cursor">|</span></p>
     </div>
 </template>
 
 <script>
-import { typeWriter } from '@/assets/js/typing.js';
-
 export default {
     data() {
         return {
-            cursorStopped: false,
+            messageActive: false,
+            message: '',
+            startMessage: 'Se vuoi, puoi lasciarmi un messaggio.',
+            speed: 45,
+            i: 0,
         }
     },
+    methods: {
+        typeMessage() {
+            if( this.i < this.startMessage.length ) {
+                this.message += this.startMessage.charAt(this.i);
+                this.i++;
+
+                setTimeout( this.typeMessage, this.speed );
+            }
+        },
+        eraseMessage() {
+            if( this.startMessage.length > 0 ) {
+                this.message = this.message.substring( 0, this.i - 1 );
+                this.i--
+
+                setTimeout( this.eraseMessage, this.speed );
+            }
+        }
+    }, 
     mounted() {
-        var i = 0;
-        var txt = 'Se vuoi, lasciami un messaggio.';
-        var elId = 'hero-content'
-        var speed = 35;
-        
-        typeWriter( txt, elId, i, speed );
-    }
+        this.typeMessage();
+    },
 }
 </script>
 
